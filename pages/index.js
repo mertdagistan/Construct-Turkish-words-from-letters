91fad3dd2ba5cd34b3cca7ab531568c616f42ab5
Mert Dağıstan <48259052+mertdagistan@users.noreply.github.com>
2023-01-16 22:43:28 +0300

    create words component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { useState } from "react";
 import classnames from "classnames";
+import Words from "@/components/words";
 
 export default function Home() {
   const [letters, setLetters] = useState("");
@@ -81,47 +82,7 @@ export default function Home() {
         </div>
 
         {words.length > 0 && (
-          <div className="flex flex-col md:flex-row flex-wrap gap-0 md:gap-5">
-            {words.map((k, index) => {
-              return (
-                <div
-                  key={k.length}
-                  className={classnames({
-                    "my-5 md:my-10 border-gray-200 border py-3 px-2 width-50": true,
-                    "bg-gray-50": index % 2 == 0,
-                  })}
-                >
-                  <label>{k.length} harfli words</label>
-
-                  <div className="flex gap-5 flex-wrap mt-3">
-                    {k.values.length > 0 &&
-                      k.values.map((v) => {
-                        return (
-                          <label
-                            className={classnames({
-                              "bg-white px-3 py-1 rounded-lg cursor-pointer hover:bg-gray-300":
-                                index % 2 == 0,
-                              "bg-gray-100 px-3 py-1 rounded-lg cursor-pointer hover:bg-gray-300":
-                                index % 2 == 1,
-                            })}
-                            key={v}
-                            onClick={() => {
-                              navigator.clipboard.writeText(v);
-                              handle_alert(
-                                `"${v}" panoya kaydedildi.`,
-                                "success"
-                              );
-                            }}
-                          >
-                            {v}
-                          </label>
-                        );
-                      })}
-                  </div>
-                </div>
-              );
-            })}
-          </div>
+         <Words words={words} alert={handle_alert}/>
         )}
       </main>
     </>
