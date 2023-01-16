1ac5f5d6b696677da540ea87aa15c12ddd936451
Mert Dağıstan <48259052+mertdagistan@users.noreply.github.com>
2023-01-16 20:16:06 +0300

    create navbar

--- components/navbar.js
+++ components/navbar.js
@@ -1,6 +1,6 @@
 function Navbar() {
   return (
-    <nav className="flex justify-center items-center h-20 border-b border-gray-100">
+    <nav className="flex justify-center items-center h-16 border-b border-gray-100">
       <h1 className="text-2xl text-gray-600 font-bold tracking-widest">Kelime Türetme</h1>
     </nav>
   );
