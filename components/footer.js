49992d2cf803c1f032b0fe641a26c623c539f490
Mert Dağıstan <48259052+mertdagistan@users.noreply.github.com>
2023-01-16 22:47:35 +0300

    add link to the footer

--- components/footer.js
+++ components/footer.js
@@ -2,7 +2,11 @@ function Footer() {
   return (
     <footer className="fixed bottom-0 left-0 right-0 h-14 bg-gray-700 flex items-center justify-center">
       <p className="text-white px-3 text-center">
-        © 2022 - 2023 mertdagistan.com - All Rights Reserved.
+        © 2022 - 2023{" "}
+        <a href="https://mertdagistan.com" target="_blank">
+          Mert DAĞISTAN
+        </a>{" "}
+        - All Rights Reserved.
       </p>
     </footer>
   );
